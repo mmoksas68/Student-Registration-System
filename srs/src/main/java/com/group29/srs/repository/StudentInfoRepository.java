70a3ef96cb7330fed72dfbfbc1535c5cec265f15
serkan delil <47603816+serkandelil@users.noreply.github.com>
2020-05-16 01:12:50 +0300

    asdasd

--- srs/src/main/java/com/group29/srs/repository/StudentInfoRepository.java
+++ srs/src/main/java/com/group29/srs/repository/StudentInfoRepository.java
@@ -20,7 +20,7 @@ public class StudentInfoRepository {
                 "INNER JOIN Member m ON u.user_id=m.user_id" +
                 "INNER JOIN Department d ON m.dept_code = d.dept_code" +
                 "INNER JOIN Phone p ON p.phone_id= u.user_id" +
-                "WHERE s.student_id =  id;";
+                "WHERE s.student_id =  id;"; asdasd
         **/
         return  jdbcTemplate.query("SELECT u.firstname, u.lastname, d.dept_name," +
                 "s.student_id, s.cgpa, s.gpa, s.current_semester," +
