28a5735b2a37839040729e95243698c51aef9420
mmoksas68 <mmusaboksas@gmail.com>
2020-05-17 20:49:59 +0300

    stash

--- srs/src/main/java/com/group29/srs/controller/StudentController.java
+++ srs/src/main/java/com/group29/srs/controller/StudentController.java
@@ -1,16 +1,11 @@
 package com.group29.srs.controller;
 
-
-import com.group29.srs.model.Student;
 import com.group29.srs.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-
 
 @Controller
 @RequestMapping("/student")
