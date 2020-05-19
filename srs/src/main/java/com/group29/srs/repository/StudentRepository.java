661bfd0cc69d2e2fe989f24a85055ecac4b2aea0
kandogu <d.kose@ug.bilkent.edu.tr>
2020-05-20 00:17:26 +0300

    Some sql mistakes fixed

--- srs/src/main/java/com/group29/srs/repository/StudentRepository.java
+++ srs/src/main/java/com/group29/srs/repository/StudentRepository.java
@@ -60,7 +60,7 @@ public class StudentRepository {
         return  jdbcTemplate.query("SELECT c.course_code, sec.section_number," +
                 " c.name,u.firstname, u.lastname,c.credits FROM Student s " +
                 "INNER JOIN Takes t ON t.s_id=s.student_id " +
-                "INNER JOIN Section sec ON t.section_id=sec.section_id AND t.course=sec.course" +
+                "INNER JOIN Section sec ON t.section_id=sec.section_id AND t.course_id=sec.course_id " +
                 "INNER JOIN Course c ON c.course_id = sec.course_id " +
                 "INNER JOIN Instructor i ON i.instructor_id= sec.teacher_id " +
                 "INNER JOIN User u ON i.instructor_id=u.user_id " +
@@ -84,7 +84,7 @@ public class StudentRepository {
                 "INNER JOIN Student s ON r.student_id = s.student_id " +
                 "INNER JOIN Assignment a ON a.assignment_id=r.assignment_id " +
                 "INNER JOIN Contains con ON con.assignment_id=a.assignment_id " +
-                "INNER JOIN Section sec ON con.section_id=sec.section_id AND con.course_id=sec.course_id" +
+                "INNER JOIN Section sec ON con.section_id=sec.section_id AND con.course_id=sec.course_id " +
                 "INNER JOIN Course c ON c.course_id = sec.course_id " +
                 "INNER JOIN Takes t on t.section_id = sec.section_id AND t.s_id = s.student_id AND t.course_id = sec.course_id "+
                 "WHERE sec.semester = ? AND s.student_id= ? AND sec.year = ?; ",new Object[] {semester, student_id, year}, new StudentGradeMapper());
